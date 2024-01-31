2fa5d43622d4ba8b602df829aa5c23022ea1e02b
емтв <user@eemptw.local>
2024-01-31 18:43:31 +0300

    initial commit of mymath package

---
+++ mymath.go
@@ -0,0 +1,20 @@
+package mymath
+
+import "math"
+
+func Sqrt(x float64) float64 {
+    return math.Sqrt(x)
+}
+
+func Sin(x float64) float64 {
+    return math.Sin(x)
+}
+
+func Cos(x float64) float64 {
+    return math.Cos(x)
+}
+
+func Pow(x, y float64) float64 {
+    return math.Pow(x, y)
+}
+
