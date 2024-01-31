a8a54339a0f640ca7470fd384a1106b6397d0158
емтв <user@eemptw.local>
2024-01-31 22:24:42 +0300

    mymath v1.0.0

--- mymath.go
+++ mymath.go
@@ -32,6 +32,8 @@ func Yn(n int, x float64) float64 {
 
 func Acosh(x float64) float64 {
     return math.Acosh(x)
-   }
-   func Asin(x float64) float64 {
+}
+   
+func Asin(x float64) float64 {
     return math.Asin(x)
+}
